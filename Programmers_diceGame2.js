f4f35e50e177eb9443165079e469a2946acf19f5
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-20 08:34:49 +0900

    refactor: programmers_기초트레이닝 (주사위 게임 2)

--- Programmers_diceGame2.js
+++ Programmers_diceGame2.js
@@ -1,16 +1,19 @@
-function solution(a, b, c) {
-  const uniqueParamCount = new Set([a, b, c]).size;
-  const sum = a + b + c;
-  const sumOfTwoSquares = a ** 2 + b ** 2 + c ** 2;
-  const sumOfCubes = a ** 3 + b ** 3 + c ** 3;
+/** FIXME:
+ * [x] 마지막 if(uniqueParamCount === 3) 조건 제거
+ * [x] 변수명 수정 uniqueParamCount, sumOfTwoSquares
+ * [x] 분기 순서를 3 -> 2 -> 1로 뒤집기
+ */
 
-  if (uniqueParamCount === 1) {
-    return sum * sumOfTwoSquares * sumOfCubes;
-  }
-  if (uniqueParamCount === 2) {
-    return sum * sumOfTwoSquares;
-  }
-  if (uniqueParamCount === 3) return sum;
+function solution(a, b, c) {
+  const kind = new Set([a, b, c]).size;
+  const sum = a + b + c;
+
+  if (kind === 3) return sum;
+
+  const sumOfSquares = a ** 2 + b ** 2 + c ** 2;
+  if (kind === 2) return sum * sumOfSquares;
+
+  return sum * sumOfSquares * (a ** 3 + b ** 3 + c ** 3);
 }
 
 console.log(solution(2, 6, 1));
