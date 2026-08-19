d1da81b5ee7dcfcc2d306448d3fd7a256e1cf67d
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-19 11:22:28 +0900

    refactor: programmers_기초트레이닝 (조건 문자열)
    중첩 if를 key로 대체

--- Programmers_conditionalString.js
+++ Programmers_conditionalString.js
@@ -1,22 +1,18 @@
 /** FIXME:
  * [x] 1. 굳이 answer가 필요없다. let 변수로 상태를 추적해야하는 번거로움이 있음
  *      -> 바로 return 하는 형태가 의도 분명해짐
- * [] 2. 중첩 if 대신 조합을 하나의 키로
- * [] 3. 더 짧게 -- '='는 같은 때도 참을 더하는 옵션일 뿐
- *
- * [] 4.
+ * [x] 2. 중첩 if 대신 조합을 하나의 키로 대체
  *
  */
+const compare = {
+  ">!": (a, b) => a > b,
+  ">=": (a, b) => a >= b,
+  "<!": (a, b) => a < b,
+  "<=": (a, b) => a <= b,
+};
+
 function solution(ineq, eq, n, m) {
-  if (ineq === ">") {
-    if (eq === "!") return Number(n > m);
-    if (eq === "=") return Number(n >= m);
-  }
-  if (ineq === "<") {
-    if (eq === "!") return Number(n < m);
-    if (eq === "=") return Number(n <= m);
-  }
-  return 0;
+  return Number(compare[ineq + eq](n, m));
 }
 
 console.log(solution("<", "=", 20, 50)); // 1
