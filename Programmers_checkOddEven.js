2aaa108ad6f921bd16be9ce3977c6b59282b2219
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-19 10:10:08 +0900

    refactor: programmers_기초트레이닝 (홀짝에 따라 다른 값 반환하기)
    - 직접 사용하지 않는 odd, even 변수 삭제
    - for문 하나로 합치기

--- Programmers_checkOddEven.js
+++ Programmers_checkOddEven.js
@@ -1,19 +1,23 @@
+/** FIXME:
+ * [x] 1. 배열이 불필요
+ * - 값을 모아뒀다가 마지막에 한번더 순회하는데, 답은 합만 필요하다.
+ *
+ * [x] 2. reduce에 초기값이 없다.
+ * 여기서는 n>= 1 이라 배열이 절대 비지 않아 통과하지만, 합계 reduce에는 ,0을 붙이는 습관이 좋다.(빈배열일 때, 초기값이 없으면 acc로 삼을 요소가 없어서 TypeError)
+ *
+ * [x] 3. 두 분기가 사실상 같은 구조이기 때문에 하나로 합치기도 가능
+ * [x] 4. odd, even 처럼 재할당하지 않는 변수는 let 보다 const가 적절함
+ *  - 배열 재할당은 주소변경이기 때문에, 주소 변경하지 않는다면 const 사용가능
+ *
+ */
 function solution(n) {
-  if (n % 2 === 1) {
-    let odd = [];
+  const isOdd = n % 2 === 1;
+  let answer = 0;
 
-    for (let i = 1; i <= n; i += 2) {
-      odd.push(i);
-    }
-    return odd.reduce((acc, cur) => acc + cur);
-  } else {
-    let even = [];
-
-    for (let i = 2; i <= n; i += 2) {
-      even.push(i * i);
-    }
-    return even.reduce((acc, cur) => acc + cur);
+  for (let i = n; i > 0; i -= 2) {
+    answer += isOdd ? i : i * i;
   }
+  return answer;
 }
 
 console.log(solution(7));
