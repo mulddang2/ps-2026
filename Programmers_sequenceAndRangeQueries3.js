3a63a14b3b62a08a82e8a1661a60c60ce47eff97
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-20 17:03:55 +0900

    refactor: programmers_기초트레이닝 (수열과 구간 쿼리 3)

--- Programmers_sequenceAndRangeQueries3.js
+++ Programmers_sequenceAndRangeQueries3.js
@@ -1,9 +1,26 @@
+/** NOTE:
+ * [x] queries[i][0]], arr[queries[i][1] 반복 접근 제거 -> 가독성 측면
+ * [x] 루프 변수 i와 문제의 i가 충돌
+ * [x] 입력 배열 arr를 직접 변경(mutation)
+ */
+
 function solution(arr, queries) {
-  for (let i = 0; i < queries.length; i++) {
-    [arr[queries[i][0]], arr[queries[i][1]]] = [
-      arr[queries[i][1]],
-      arr[queries[i][0]],
-    ];
+  const result = [...arr];
+
+  for (const [i, j] of queries) {
+    [result[i], result[j]] = [result[j], result[i]];
   }
-  return arr;
+
+  return result;
 }
+
+console.log(
+  solution(
+    [0, 1, 2, 3, 4],
+    [
+      [0, 3],
+      [1, 2],
+      [1, 4],
+    ],
+  ),
+);
