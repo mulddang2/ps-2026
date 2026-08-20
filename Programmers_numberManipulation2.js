96a359f875dac02eb3c29346bfe3babd22b2f7be
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-20 15:06:25 +0900

    refactor: programmers_기초트레이닝 (수 조작하기 2)

--- Programmers_numberManipulation2.js
+++ Programmers_numberManipulation2.js
@@ -1,14 +1,21 @@
-const DELTA = {
-  1: "w",
-  "-1": "s",
-  10: "d",
-  "-10": "a",
+/** FIXME:
+ * [x] 객체 키 표기 통일
+ * [x] DELTA 변수명 명확하게 수정
+ * [x] 루프 인덱스 기준을 문제 설명과 맞추기
+ * [x] 미정의 키 처리 - 다른 값 들어오면 undefined가 문자열에 그대로 붙음
+ */
+
+const DELTA_TO_KEY = {
+  '1': 'w',
+  '-1': 's',
+  '10': 'd',
+  '-10': 'a',
 };
 
 function solution(numLog) {
-  let control = "";
-  for (let i = 0; i < numLog.length - 1; i++) {
-    control += DELTA[numLog[i + 1] - numLog[i]];
+  let control = '';
+  for (let i = 1; i < numLog.length; i++) {
+    control += DELTA_TO_KEY[numLog[i] - numLog[i - 1]];
   }
   return control;
 }
