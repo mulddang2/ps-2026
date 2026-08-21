21be094479a156961b86d7b3956628c675d4ab4b
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-21 15:22:21 +0900

    refactor: programmers_기초트레이닝 (콜라츠 수열 만들기)

--- Programmers_makingACollatzSequence.js
+++ Programmers_makingACollatzSequence.js
@@ -1,14 +1,15 @@
+/** NOTE:
+ * [x] result.push(n) 중복삭제
+ * [x] 매개변수 n을 직접 재할당 -> 별도 변수로 수정
+ * [x] 조건을 긍정형으로 변경 (짝수 먼저)
+ */
 function solution(n) {
-  const result = [n];
+  let x = n
+  const result = [x];
 
-  while (n !== 1) {
-    if (n % 2 !== 0) {
-      n = 3 * n + 1;
-      result.push(n);
-    } else {
-      n = n / 2;
-      result.push(n);
-    }
+  while (x !== 1) {
+    x = x % 2 === 0 ? x / 2 : 3 * x + 1
+    result.push(x);
   }
   return result;
 }
