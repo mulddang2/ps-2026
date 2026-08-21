4035af14916509e92c097a28ae144a308e7e0353
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-21 10:34:34 +0900

    refactor: programmers_기초트레이닝 (수열과 구간 쿼리 4)

--- Programmers_sequenceAndRangeQueries4.js
+++ Programmers_sequenceAndRangeQueries4.js
@@ -1,8 +1,14 @@
+/** NOTE:
+ * [x] for문 내에서 let 선언 안함
+ * [x] 문제 제한 사항에 0 <= k가 있어서 k=0이 들어올 수 있는 부분을 코드에 의도를 드러내기
+ * [x] for문 루프를 k의 배수로 구하고 k씩 건너뛰어 성능개선
+ */
 function solution(arr, queries) {
     const result = [...arr];
     for (const [s, e, k] of queries) {
-        for (i = s; i <= e; i++) {
-            if (i % k === 0) result[i] += 1;
+        if (k === 0) continue;
+        for (let i = Math.ceil(s / k) * k; i <= e; i += k) {
+            result[i] += 1;
         }
     }
     return result;
