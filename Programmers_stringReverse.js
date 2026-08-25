95840e1c8f63d2225c0596de391c279aa567a4c1
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-25 11:46:06 +0900

    refactor: programmers_기초트레이닝 (문자열 뒤집기)

--- Programmers_stringReverse.js
+++ Programmers_stringReverse.js
@@ -1,11 +1,10 @@
+/** NOTE:
+ * [x]  .split('') → [...str] 스프레드로 교체
+ */
 function solution(my_string, s, e) {
-  return (
-    my_string.slice(0, s) +
-    my_string
-      .slice(s, e + 1)
-      .split('')
-      .reverse()
-      .join('') +
-    my_string.slice(e + 1)
-  );
+  const reversed = [...my_string.slice(s, e + 1)].reverse().join('');
+  return my_string.slice(0, s) + reversed + my_string.slice(e + 1);
 }
+
+console.log(solution('Progra21Sremm3', 6, 12));
+console.log(solution('Stanley1yelnatS', 4, 10, 'Stanley1yelnatS'));
