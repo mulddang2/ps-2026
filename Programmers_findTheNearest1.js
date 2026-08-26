4d257b86cf64d410c8ef21ddb0db1a129d39fb3f
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-26 14:39:17 +0900

    refactor: programmers_기초트레이닝 (가까운 1 찾기)

--- Programmers_findTheNearest1.js
+++ Programmers_findTheNearest1.js
@@ -1,7 +1,8 @@
+/** NOTE:
+ * [x] else if (...) continue;는 죽은 코드 -- 어차피 if에서 return하지 않으면 다음 반복으로 넘어가므로 continue는 아무 효과가 없다.
+ * [x] i !== arr.length는 항상 참이므로 불필요한 조건
+ * [x] arr[i] === 0 불필요
+ */
 function solution(arr, idx) {
-  for (let i = idx; i < arr.length; i++) {
-    if (arr[i] === 1) return i;
-    else if (arr[i] === 0 && i !== arr.length) continue;
-  }
-  return -1;
+  return arr.indexOf(1, idx);
 }
