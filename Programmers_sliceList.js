72d3dc63d5c75dfc55cf36f3483d280d042fd750
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-26 15:48:47 +0900

    refactor: programmers_기초트레이닝 (리스트 자르기)

--- Programmers_sliceList.js
+++ Programmers_sliceList.js
@@ -1,22 +1,27 @@
+/** NOTE:
+ * [x] 불필요한 바깥 for 루프 제거 (빈배열일 경우 undefined 반환)
+ * [x] if 4개 -> switch(n) 또는 if/else if로 변경하기
+ * [x] n이 1~4 밖일 때의 반환값이 없다. -> default 고려
+ * [x] intervalList는 n===4일 때만 쓰이므로 해당 분기 안으로 옮기기
+ */
 function solution(n, slicer, num_list) {
   const [a, b, c] = slicer;
-  const intervalList = [];
-  for (let i = 0; i < num_list.length; i++) {
-    if (n === 1) {
+  switch (n) {
+    case 1:
       return num_list.slice(0, b + 1);
-    }
-    if (n === 2) {
+    case 2:
       return num_list.slice(a);
-    }
-    if (n === 3) {
+    case 3:
       return num_list.slice(a, b + 1);
-    }
-    if (n === 4) {
+    case 4: {
+      const intervalList = [];
       for (let i = a; i <= b; i += c) {
         intervalList.push(num_list[i]);
       }
       return intervalList;
     }
+    default:
+      return [];
   }
 }
 
