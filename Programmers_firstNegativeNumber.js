130238eb2bb9e80440ea65609ffef144c3925c47
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-26 16:07:45 +0900

    feat: programmers_기초트레이닝 (첫번째로 나오는 음수)

---
+++ Programmers_firstNegativeNumber.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+  for (let i = 0; i < num_list.length; i++) {
+    if (num_list[i] < 0) return i;
+  }
+  return -1;
+}
+
+console.log(solution([12, 4, 15, 46, 38, -2, 15]));
+console.log(solution([13, 22, 53, 24, 15, 6]));
