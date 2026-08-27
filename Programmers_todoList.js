0e32bb9975f58bfbc22ba529b78c009a86f4e813
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-27 19:41:25 +0900

    refactor: programmers_기초트레이닝 (할 일 목록)

--- Programmers_todoList.js
+++ Programmers_todoList.js
@@ -1,9 +1,9 @@
+/** NOTE:
+ * [x] filter로 변경가능
+ * [x] 순회 대상이 반대 -- 반환값은 todo_list 인데, finished를 순회하고 있어, 인덱스로 todo_list를 역참조하고 있다. 이보다는, todo_list를 순회하고, finished[i]를 조건으로 쓰는 것이 더 자연스럽다.
+ * [x] v===false -> !v -- finished는 boolean 배열임이 보장되기 때문에,불필요
+ * [x] result 변수명 -- unfinished 같은 이름으로 변경해도 좋을 듯함.
+ */
 function solution(todo_list, finished) {
-  const result = [];
-  finished.forEach((v, i) => {
-    if (v === false) {
-      result.push(todo_list[i]);
-    }
-  });
-  return result;
+  return todo_list.filter((_, i) => !finished[i]);
 }
