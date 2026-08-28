11807b4bb01720dcee899f7740f6aff141ec4e79
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-28 11:23:57 +0900

    refactor: programmers_기초트레이닝 (1로 만들기)

--- Programmers_MakeIt1.js
+++ Programmers_MakeIt1.js
@@ -1,13 +1,23 @@
-function solution(num_list) {
-  let count = 0;
+/** NOTE:
+ * [x] 삼항 연산 부분은 같은 연산이다. Math.floor로 묶기 가능
+ * [x] 콜백 매개변수를 재할당 -- forEach 파라미터 v를 루프 변수로 덮어쓰고 있다. (입력 원소와 작업용 변수가 섞여 읽기 불편)
+ * [x] 외부 변수 누적 대신 reduce 사용하기
+ * [x] while 대신 수식으로 바로 계산 가능(선택)
+ * [x] 방어적 종료 조건 (선택) -- while (v!==0) 은 v가 0이나 음수면 무한 루프 이기 때문에, v > 1 쓰는게 더 안정적
+ */
 
-  num_list.forEach((v) => {
-    while (v !== 1) {
-      v = v % 2 === 0 ? v / 2 : (v - 1) / 2;
-      count++;
-    }
-  });
-  return count;
+function countSteps(num) {
+  let n = num;
+  let steps = 0;
+  while (n > 1) {
+    n = Math.floor(n / 2);
+    steps++;
+  }
+  return steps;
+}
+
+function solution(num_list) {
+  return num_list.reduce((total, n) => total + countSteps(n), 0);
 }
 
 console.log(solution([12, 4, 15, 1, 14]));
