435d31726e453d24e1bbc2ed924185dbbb65ebaf
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-28 17:31:21 +0900

    refactor: programmers_기초트레이닝 (조건에 맞게 수열 변환하기 1)

--- Programmers_transformSequenceToFitCondition1.js
+++ Programmers_transformSequenceToFitCondition1.js
@@ -1,11 +1,16 @@
+/** NOTE:
+ * [x] i 미사용 매개변수 지우기
+ * [x] let cur = v 불필요
+ * [x] return (cur /= 2) -- 반환값 자리에서 대입 불필요
+ * [x] 중괄호 일관성 있게 하기
+ * [x] 조건식 단순화 -- v % 2 === 0 아니면 자동으로 홀수이기 때문에 삼항 연산자로 수정해보기
+ *
+ *
+ */
 function solution(arr) {
-  return arr.map((v, i) => {
-    let cur = v;
-    if (cur >= 50 && cur % 2 === 0) {
-      return (cur /= 2);
-    } else if (cur < 50 && cur % 2 !== 0) {
-      return cur * 2;
-    } else return cur;
+  return arr.map((v) => {
+    if (v % 2 === 0) return v >= 50 ? v / 2 : v;
+    return v < 50 ? v * 2 : v;
   });
 }
 
