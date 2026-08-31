6ae503be1256f68604a93eca7cc5336b0a6818cd
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-31 15:52:08 +0900

    refactor: programmers_기초트레이닝 (수열과 구간 쿼리 1)

--- Programmers_sequenceAndRangeQueries1.js
+++ Programmers_sequenceAndRangeQueries1.js
@@ -1,21 +1,44 @@
+/** NOTE:
+ * [x] 이중 반복문 -> 차분 배열 + 누적합으로 교체 개선
+ * [x] 차분 배열 길이를 arr.length + 1로 잡아 diff[e + 1] 범위 초과 방지
+ * [x] 결과 생성은 map으로 처리해 [...arr] 복사 단계 제거
+ * [x] 엣지 케이스 확인 s===e(단일 원소), e === arr.length - 1 (마지막 인덱스), 쿼리 1개
+ * [x] !!!!구간 업데이트 = 차분 배열 패턴!!!!
+ */
 function solution(arr, queries) {
-  const result = [...arr];
+  const diff = new Array(arr.length + 1).fill(0);
 
   for (const [s, e] of queries) {
-    for (let i = s; i <= e; i++) {
-      result[i]++;
-    }
+    diff[s] += 1;
+    diff[e + 1] -= 1;
   }
-  return result;
+
+  // acc: 몇개의 쿼리 구간에 겹쳐있는가
+  let acc = 0;
+  return arr.map((v, i) => {
+    acc += diff[i];
+    return v + acc;
+  });
 }
+
+// console.log(
+//   solution(
+//     [0, 1, 2, 3, 4],
+//     [
+//       [0, 1],
+//       [1, 2],
+//       [2, 3],
+//     ],
+//   ),
+// );
 
 console.log(
   solution(
     [0, 1, 2, 3, 4],
     [
-      [0, 1],
-      [1, 2],
-      [2, 3],
+      [1, 1],
+      [2, 2],
+      [4, 4],
     ],
   ),
 );
