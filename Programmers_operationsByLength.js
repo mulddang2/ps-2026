16e0d1091c98eead424d55e2d1f943af0621973a
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-08-31 17:30:56 +0900

    refactor: programmers_기초트레이닝 (길이에 따른 연산)

--- Programmers_operationsByLength.js
+++ Programmers_operationsByLength.js
@@ -1,11 +1,14 @@
+/** NOTE:
+ * [x] 화살표 함수의 암시적 반환(Implicit Return) 활용하기
+ * [x] 중괄호 {}와 return 키워드를 생략하면 코드를 훨씬 간결하게 작성할 수 있습니다.
+- AS-IS: (sum, cur) => { return sum + cur; }
+- TO-BE: (sum, cur) => sum + cur
+ * [x] 두 번째 reduce에 초기값 명시하기
+ */
 function solution(num_list) {
   return num_list.length >= 11
-    ? num_list.reduce((sum, cur) => {
-        return sum + cur;
-      }, 0)
-    : num_list.reduce((multiply, cur) => {
-        return multiply * cur;
-      });
+    ? num_list.reduce((sum, cur) => sum + cur, 0)
+    : num_list.reduce((multiply, cur) => multiply * cur, 1);
 }
 
 console.log(solution([3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1]));
