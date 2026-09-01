6e99edc7dfaecd1d2265c36b4236507f707f62e2
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-01 19:12:41 +0900

    refactor: programmers_기초트레이닝 (문자열 바꿔서 찾기)

--- Programmers_replaceAndFindString.js
+++ Programmers_replaceAndFindString.js
@@ -1,6 +1,10 @@
+/** NOTE:
+ * [x] 길이가 비교적 짧은 pet을 뒤집어 myString과 비교해도 됨
+ * [x] 삼항 연산자 대신 숫자 형변환(+) 활용
+ */
 function solution(myString, pat) {
-  const transStr = [...myString].map((v) => (v === 'A' ? 'B' : 'A')).join('');
-  return transStr.includes(pat) ? 1 : 0;
+  const transPat = [...pat].map((v) => (v === 'A' ? 'B' : 'A')).join('');
+  return +myString.includes(transPat);
 }
 
 console.log(solution('ABBAA', 'AABB'));
