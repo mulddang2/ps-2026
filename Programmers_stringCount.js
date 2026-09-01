cfd1e4bb47c4f4892da375684ce55db58cfbe961
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-01 17:04:16 +0900

    refactor: programmers_기초트레이닝 (문자열이 몇 번 등장하는지 세기)

--- Programmers_stringCount.js
+++ Programmers_stringCount.js
@@ -1,8 +1,14 @@
+/** NOTE:
+ * [x] 반복문 범위 최적화: i < myString.length 대신 i <= myString.length - pat.length 사용하여 불필요한 루프 제외하기
+ * [x] 가독성 향상: slice(i, i + pat.length) === pat 대신 myString.startsWith(pat, i) 메서드 활용 고려해보기
+ * [x] 증감 연산자 단축: count += 1 대신 count++ 사용하기 (취향/컨벤션 차이)
+ */
+
 function solution(myString, pat) {
   let count = 0;
-  for (let i = 0; i < myString.length; i++) {
-    if (myString.slice(i, i + pat.length) === pat) {
-      count += 1;
+  for (let i = 0; i <= myString.length - pat.length; i++) {
+    if (myString.startsWith(pat, i)) {
+      count++;
     }
   }
   return count;
