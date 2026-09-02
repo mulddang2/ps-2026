05eeec03304578a735b6e6c460d60ab84f3bfef5
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-02 17:29:24 +0900

    refactor: programmers_기초트레이닝 (배열의 길이를 2의 거듭제곱으로 만들기)

--- Programmers_makePowerOfTwo.js.js
+++ Programmers_makePowerOfTwo.js.js
@@ -1,3 +1,7 @@
+/** NOTE:
+ * [x] 불필요한 메모리 할당 최소화 -- concat메서드는 이미 새 배열을 생성해서 반환하기 때문에 불필요한 배열 생성 연산이 1회 더 추가 되어 있는 [...arr] 삭제
+ *
+ */
 function solution(arr) {
   let targetLength = 1;
 
@@ -5,7 +9,7 @@ function solution(arr) {
   while (targetLength < arr.length) {
     targetLength *= 2;
   }
-  return [...arr].concat(Array(targetLength - arr.length).fill(0));
+  return arr.concat(Array(targetLength - arr.length).fill(0));
 }
 
 console.log(solution([1, 2, 3, 4, 5, 6]));
