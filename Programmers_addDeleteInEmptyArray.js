22ce076fab204be04ca79815ab6a96ad8fc473f0
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-02 15:06:15 +0900

    refactor: programmers_기초트레이닝 (빈 배열에 추가, 삭제하기)

--- Programmers_addDeleteInEmptyArray.js
+++ Programmers_addDeleteInEmptyArray.js
@@ -1,18 +1,16 @@
-function solution(arr, flag) {
-  let X = [];
+/** NOTE:
+ * [x] 배열 생성 단순화: for 문으로 push()를 반복하는 대신 Array().fill()과 펼침 연산자(...)를 활용해 보았는가?
+ * [x] 배열 삭제 단순화: for 문으로 pop()을 반복하는 대신 splice()나 slice()를 활용해 단일 연산으로 처리해 보았는가?
+ * [x] 고차 함수 활용: Index 접근 중심의 for 문을 forEach 또는 reduce로 대체하여 함수형 코드로 작성해 보았는가?
+ *
+ */
 
-  for (let i = 0; i < arr.length; i++) {
-    if (flag[i]) {
-      for (let j = 0; j < arr[i] * 2; j++) {
-        X.push(arr[i]);
-      }
-    } else {
-      for (let j = 0; j < arr[i]; j++) {
-        X.pop();
-      }
-    }
-  }
-  return X;
+function solution(arr, flag) {
+  return flag.reduce((acc, f, i) => {
+    return f
+      ? [...acc, ...Array(arr[i] * 2).fill(arr[i])]
+      : acc.slice(0, acc.length - arr[i]);
+  }, []);
 }
 
 console.log(solution([3, 2, 4, 1, 3], [true, false, true, false, false]));
