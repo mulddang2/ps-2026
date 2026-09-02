2dd3d0fe54a4d8dca8d34dccee28e4173ba1f2b2
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-02 16:55:14 +0900

    fix: programmers_기초트레이닝 (무작위로 K개의 수 뽑기)
    - push로 인한 사이드 이펙트 오류 수정

--- Programmers_selectRandomKNumbers.js
+++ Programmers_selectRandomKNumbers.js
@@ -1,13 +1,11 @@
+/** NOTE:
+ * [x] uniqueArr.length가 push로 변경되기 때문에, 변경되기 전에 추가해야할 갯수를 미리 담아놓아야한다.
+ * [x] Array.fill()과 concat() 활용해서 수정
+ */
 function solution(arr, k) {
   const uniqueArr = [...new Set(arr)];
-  if (uniqueArr.length > k) return uniqueArr.slice(0, k);
-  else if (uniqueArr.length === k) return uniqueArr;
-  else {
-    for (let i = 0; i <= k - uniqueArr.length; i++) {
-      uniqueArr.push(-1);
-    }
-    return uniqueArr;
-  }
+  if (uniqueArr.length >= k) return uniqueArr.slice(0, k);
+  return uniqueArr.concat(Array(k - uniqueArr.length).fill(-1));
 }
 
 console.log(solution([0, 1, 1, 2, 2, 3], 3));
