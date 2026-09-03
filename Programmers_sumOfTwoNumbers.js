5f2b9a5dac67c7a352c3cb60c660bf76028747fa
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-03 20:13:22 +0900

    refactor: programmers_기초트레이닝 (두 수의 합)

--- Programmers_sumOfTwoNumbers.js
+++ Programmers_sumOfTwoNumbers.js
@@ -1,7 +1,8 @@
+/** NOTE:
+ * [x] Number(a) + Number(b) 오차 발생 이유 -- js에서 Number의 안전 한계 '약 16'자리까지 인데, 해당 문제의 입력 크기는 최대 '10만'자리까지여서 오차발생
+ */
 function solution(a, b) {
-  const sum = BigInt(a) + BigInt(b);
-
-  return sum.toString();
+  return (BigInt(a) + BigInt(b)).toString();
 }
 
 console.log(solution('582', '734'));
