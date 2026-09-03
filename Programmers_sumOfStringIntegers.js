f9fb89243350f03eacd1ef7d104591db2858d2a5
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-03 17:47:51 +0900

    refactor: programmers_기초트레이닝 (문자열 정수의 합)

--- Programmers_sumOfStringIntegers.js
+++ Programmers_sumOfStringIntegers.js
@@ -1,5 +1,10 @@
+/** NOTE:
+ * [x] 단일 순회 방식으로 로직 합치기 -- map으로 새로운 배열 만든 뒤, reduce를 다시 호출하고 있어, 문자열 전체를 총 2번 순회하여 중간 배열을 추가로 생성하고 있는 문제가 있음
+ * [x] 문자열 다루기 최적화하기 -- 불필요한 배열 변환 방지하기
+ */
+
 function solution(num_str) {
-  return [...num_str].map((v) => Number(v)).reduce((acc, cur) => acc + cur, 0);
+  return [...num_str].reduce((acc, cur) => acc + Number(cur), 0);
 }
 
 console.log(solution('123456789'));
