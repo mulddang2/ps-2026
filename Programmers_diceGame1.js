a9319f76ab6fe284ba78f09e803c261744dd1485
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-04 14:31:37 +0900

    refactor: programmers_기초트레이닝 (주사위 게임 1)

--- Programmers_diceGame1.js
+++ Programmers_diceGame1.js
@@ -1,12 +1,13 @@
+/** NOTE:
+ * [x] isOneOdd 변수의 논리 오류 가능성 -- a % 2 !== 0 || b % 2 !== 0 표현식은 둘다 홀수 일 때도 true이기 때문에 논리가 안맞음. --> 하나만 홀수 일 때는, (a % 2) !== (b % 2) 형식이 정확함.
+ * [x] 홀수의 개수를 기준으로 조건 분기 작성해보기
+ */
 function solution(a, b) {
-  const isBothOdd = a % 2 !== 0 && b % 2 !== 0;
-  const isOneOdd = a % 2 !== 0 || b % 2 !== 0;
+  const oddCount = (a % 2 !== 0) + (b % 2 !== 0);
 
-  if (isBothOdd) {
-    return a ** 2 + b ** 2;
-  } else if (isOneOdd) {
-    return 2 * (a + b);
-  } else return Math.abs(a - b);
+  if (oddCount === 2) return a ** 2 + b ** 2;
+  if (oddCount === 1) return 2 * (a + b);
+  return Math.abs(a - b);
 }
 
 console.log(solution(3, 5));
