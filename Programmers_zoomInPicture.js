37885312ec21efc56b5b9bffe6d8dad86ce102ae
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-04 18:32:36 +0900

    refactor: programmers_기초트레이닝 (그림 확대)

--- Programmers_zoomInPicture.js
+++ Programmers_zoomInPicture.js
@@ -1,12 +1,15 @@
+/** NOTE:
+ * [x] 정규식을 활용해서 개선해보기 -- split('') 및 map() 연산 과정에서 발생하는 중간 배열 생성을 생략하고, 문자열 단위에서 즉시 대체 연산을 수행하기 떄문에 메모리 생성 오버헤드가 줄어듦
+ */
+
 function solution(picture, k) {
   const result = [];
 
-  for (let i = 0; i < picture.length; i++) {
-    const expandedRow = picture[i]
-      .split('')
-      .map((char) => char.repeat(k))
-      .join('');
+  for (const row of picture) {
+    // 모든 문자를 k번 반복
+    const expandedRow = row.replace(/./g, (char) => char.repeat(k));
 
+    // 세로 k번 추가
     for (let j = 0; j < k; j++) {
       result.push(expandedRow);
     }
