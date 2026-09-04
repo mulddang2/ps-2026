82cf55d0e7bbc2a4cfc0a499684237b28574a1c2
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-04 16:02:48 +0900

    refactor: programmers_기초트레이닝 (커피 심부름)

--- Programmers_coffeRun.js
+++ Programmers_coffeRun.js
@@ -1,14 +1,18 @@
+/** NOTE:
+ * [x] 문자열 조건 최소화하기 -- anything과 americano가 동일한 가격 구조를 가지기 때문에, 차이가 나은 'latte' 조건 하나로만 분기 로직 단축하기
+ * [x] 기본 루프문 활용으로 실행 속도 최적화하기
+ *
+ */
 function solution(order) {
-  let totalCount = 0;
-
-  for (const menu of order) {
-    if (menu.includes('ame')) {
-      totalCount += 4500;
-    } else if (menu.includes('latte')) {
-      totalCount += 5000;
-    } else totalCount += 4500;
+  let total = 0;
+  for (let i = 0; i < order.length; i++) {
+    if (order[i].includes('latte')) {
+      total += 5000;
+    } else {
+      total += 4500;
+    }
   }
-  return totalCount;
+  return total;
 }
 
 console.log(
