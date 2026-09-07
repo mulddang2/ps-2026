ad614c07193bd6da3c0831ec0e5a068fe62f8d7c
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-07 10:59:27 +0900

    refactor: programmers_기초트레이닝 (조건에 맞게 수열 변환하기 3)

--- Programmers_transformSequenceToFitCondition3.js
+++ Programmers_transformSequenceToFitCondition3.js
@@ -1,5 +1,8 @@
+/** NOTE:
+ * [x] 콜백 함수 내 재할당 연산 제거하기 -- 내장 메서드 사용 시 매개변수에 할당 연산자를 사용하지 않고 순수 결괏값만 반환하도록 수정하기
+ */
 function solution(arr, k) {
-  return arr.map((num) => (k % 2 === 0 ? (num += k) : (num *= k)));
+  return arr.map((num) => (k % 2 === 0 ? num + k : num * k));
 }
 
 console.log(solution([1, 2, 3, 100, 99, 98], 3));
