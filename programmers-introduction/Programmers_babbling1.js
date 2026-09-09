61171f34f4dd63b9798fa1dcb2a30484bfe5ebea
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-09 17:11:35 +0900

    refactor: programmers_입문 (옹알이 1)

--- programmers-introduction/Programmers_babbling1.js
+++ programmers-introduction/Programmers_babbling1.js
@@ -1,14 +1,40 @@
+/** NOTE:
+ * [x] replaceAll과 trim을 반복호출해서 새 문자열 생성하는 대신, startsWith와 인덱스 포인터 활용하여 메모리 할당 없이 탐색하기
+ * [x] 치환 공백으로 인한 엣지 케이스 해결 -- ('mayawoo') 에서 오답
+ * [x] 정규표현식 이용한 선언적 검증 로직 적용 -- test() 함수 활용
+ */
+
 function solution(babbling) {
-  const soundList = ['aya', 'ye', 'woo', 'ma'];
-  let totalCount = 0;
+  // 1. 인덱스 포인터 기반 탐색 (startsWith 활용)
+  let count = 0;
+  const sounds = ['aya', 'ye', 'woo', 'ma'];
+
   for (const word of babbling) {
-    let currentWord = word;
-    for (const sound of soundList) {
-      currentWord = currentWord.replaceAll(sound, ' ').trim();
+    let i = 0;
+    let isValid = true;
+
+    while (i < word.length) {
+      let matched = false;
+      for (const sound of sounds) {
+        if (word.startsWith(sound, i)) {
+          i += sound.length;
+          matched = true;
+          break;
+        }
+      }
+      // 4가지 발음 중 어떤 것도 매칭되지 않으면 실패
+      if (!matched) {
+        isValid = false;
+        break;
+      }
     }
-    if (currentWord.length === 0) totalCount += 1;
+    if (isValid) count++;
   }
-  return totalCount;
+  return count;
+
+  // 2. 정규표현식 활용
+  // const regex = /^(aya|ye|woo|ma)+$/;
+  // return babbling.filter(word => regex.test(word).length;
 }
 
 console.log(solution(['aya', 'yee', 'u', 'maa', 'wyeoo']));
