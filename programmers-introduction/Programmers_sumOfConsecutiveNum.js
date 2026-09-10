d0d0e9462a8d5fd2d4fb50a544bb0ea9d8e8a9d8
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-10 16:27:37 +0900

    refactor: programmers_입문 (연속된 수의 합)

--- programmers-introduction/Programmers_sumOfConsecutiveNum.js
+++ programmers-introduction/Programmers_sumOfConsecutiveNum.js
@@ -1,7 +1,16 @@
-function solution(num, total) {
-  const firstPoint = (total - ((num - 1) * num) / 2) / num;
+/** NOTE:
+ * [x] new Array~ 부분에서 fill로 배열 전체 초기화한 뒤 map으로 새로운 배열을 다시 생성하는 구조 -> 불필요한 중간 객체 생성으로 GC 부화를 일으킴
+ *
+ */
 
-  const result = new Array(num).fill(firstPoint).map((n, idx) => n + idx);
+function solution(num, total) {
+  const start = (total - ((num - 1) * num) / 2) / num;
+
+  const result = new Array(num);
+
+  for (let i = 0; i < num; i++) {
+    result[i] = start + i;
+  }
 
   return result;
 }
