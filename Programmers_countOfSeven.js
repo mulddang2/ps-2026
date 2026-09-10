d2eeea25bebfb0af7b9b9291a8fe12675db687b8
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-10 19:39:26 +0900

    refactor: programmers_입문 (7의 개수)

--- Programmers_countOfSeven.js
+++ Programmers_countOfSeven.js
@@ -1,8 +1,16 @@
+/** NOTE:
+ * - array.toString()은 배열 내 모든 숫자와 구분자가 포함된 대형 문자열을 힙 메모리에 새로 생성해서 데이터 크기가 커짐
+ * [x] 문자열 변환 대신 수학적 산술 연산 적용하기
+ */
 function solution(array) {
-  const strArr = array.toString();
   let count = 0;
-  for (const str of strArr) {
-    if (str === '7') count++;
+
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    while (num > 0) {
+      if (num % 10 === 7) count++;
+      num = Math.floor(num / 10);
+    }
   }
   return count;
 }
