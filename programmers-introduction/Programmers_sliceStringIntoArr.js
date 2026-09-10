0a198604670ee48226e39f3081fe5af8c570144d
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-10 18:37:06 +0900

    refactor: programmers_입문 (잘라서 배열로 저장하기)

--- programmers-introduction/Programmers_sliceStringIntoArr.js
+++ programmers-introduction/Programmers_sliceStringIntoArr.js
@@ -1,8 +1,16 @@
-function solution(my_str, n) {
-  const result = [];
+/** NOTE:
+ * [x] Math.ceil() 계산으로 결과 배열의 정적 크기 초기화
+ * [x] push 메서드 대신 인덱스 직접 할당 방식으로 메모리 재배치 오버헤드 방지
+ */
 
-  for (let i = 0; i < my_str.length; i += n) {
-    result.push(my_str.slice(i, i + n));
+function solution(my_str, n) {
+  const len = my_str.length;
+  const count = Math.ceil(len / n);
+  const result = new Array(count);
+
+  for (let i = 0; i < count; i++) {
+    const start = i * n;
+    result[i] = my_str.slice(start, start + n);
   }
   return result;
 }
