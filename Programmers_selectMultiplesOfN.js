d3b775cc218739d12de99d15cfbedfd13acccf05
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-11 16:44:19 +0900

    feat: programmers_입문 (n의 배수 고르기)

---
+++ Programmers_selectMultiplesOfN.js
@@ -0,0 +1,9 @@
+function solution(n, numlist) {
+  return numlist.filter((v) => {
+    if (v % n === 0) return v;
+  });
+}
+
+console.log(solution(3, [4, 5, 6, 7, 8, 9, 10, 11, 12]));
+console.log(solution(5, [1, 9, 3, 10, 13, 5]));
+console.log(solution(12, [2, 100, 120, 600, 12, 12]));
