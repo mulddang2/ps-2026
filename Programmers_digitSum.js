8c7e043ca49f75fb32f9c359984f8cf3718f8ab9
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-11 16:21:08 +0900

    refactor: programmers_입문 (자릿수 더하기)

--- Programmers_digitSum.js
+++ Programmers_digitSum.js
@@ -1,10 +1,18 @@
+/** NOTE:
+ * [x] String 변환 및 split()을 이용한 동적 배열 생성 구문을 피하고, 기본 숫자 타입만 활용하기
+ * [x] 수학적 연산 알고리즘으로 전환 -- 나머지 연산자와 몫 활용
+ * [x] 중복 단항 연산자 및 형변환 정리
+ * [x] 경계 조건 엣지 케이스 검증 -- 입력값 0처리 확인
+ */
 function solution(n) {
-  const stringN = String(n).split('');
+  let sum = 0;
 
-  return stringN.reduce((acc, cur) => +acc + +cur, 0);
+  while (n > 0) {
+    sum += n % 10;
+    n = Math.floor(n / 10);
+  }
+  return sum;
 }
 
 console.log(solution(1234));
 console.log(solution(930211));
-console.log(solution(110000));
-console.log(solution(500050));
