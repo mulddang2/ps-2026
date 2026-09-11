f43783bf9857d89fcedb71f764f7998fbcc9645e
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-11 10:46:03 +0900

    refactor: programmers_입문 (세균 증식)

--- Programmers_bacterialGrowth.js
+++ Programmers_bacterialGrowth.js
@@ -1,11 +1,13 @@
+/** NOTE:
+ * [x] 반복문 대신 단항/이항 수학 연산식으로 전환하기
+ * [x] 비트 연산자 활용 검토 -- 2의 거듭제곱 관련 문제에서 시프트 연산 적용
+ * [x] 변수 최소화
+ */
 function solution(n, t) {
-  let count = n;
-  let i = 0;
-  while (i < t) {
-    count = count * 2;
-    i++;
-  }
-  return count;
+  // 1. 비트 왼쪽 시프트 연산 활용
+  // return n << t;
+  // 2. 거듭제곱 연산자 활용
+  return n * 2 ** t;
 }
 
 console.log(solution(2, 10));
