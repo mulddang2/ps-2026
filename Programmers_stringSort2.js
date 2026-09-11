e9d388118c7c9212216c43196f8dca8346ed9d86
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-11 09:38:05 +0900

    refactor: programmers_입문 (문자열 정렬하기 2)

--- Programmers_stringSort2.js
+++ Programmers_stringSort2.js
@@ -1,9 +1,9 @@
+/** NOTE:
+ * [x] 루프 내부에서 개별 소문자 변환 제거 --> my_string.toLowerCase()로 1회만 호출
+ * [x]  문자열 덧셈 연산의 누적 삭제
+ */
 function solution(my_string) {
-  let result = '';
-  for (const s of my_string) {
-    result += s.toLowerCase();
-  }
-  return [...result].sort().join('');
+  return [...my_string.toLowerCase()].sort().join('');
 }
 
 console.log(solution('Bcad'));
