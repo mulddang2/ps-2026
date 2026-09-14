819a4cc0828cc9ae9d636fd8320aab61aa418b03
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-14 16:31:22 +0900

    refactor: programmers_입문 (가장 큰 수 찾기)

--- Programmers_maxNumbersearch.js
+++ Programmers_maxNumbersearch.js
@@ -1,8 +1,18 @@
+/** NOTE:
+ * [x] 대규모 배열 입력 시 Math.max(...array) 대신 단일 루프 적용하기
+ * [x] 단일 순회 알고리즘 적용하기(하나의 반복문 안에서 최댓값과 인덱스를 동시 갱신하기)
+ */
 function solution(array) {
-  const maxNumber = Math.max(...array);
-  const maxIndex = array.indexOf(maxNumber);
-  const result = [maxNumber, maxIndex];
-  return result;
+  let maxNumber = array[0];
+  let maxIndex = 0;
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maxNumber) {
+      maxNumber = array[i];
+      maxIndex = i;
+    }
+  }
+  return [maxNumber, maxIndex];
 }
 
 console.log(solution([1, 8, 3]));
