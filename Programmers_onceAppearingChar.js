94cb7bfd0c5c12b4aa0e3c6041ac1678c99fa4b7
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-14 17:42:03 +0900

    refactor: programmers_입문 (한 번만 등장한 문자)

--- Programmers_onceAppearingChar.js
+++ Programmers_onceAppearingChar.js
@@ -1,11 +1,25 @@
+/** NOTE:
+ * [x] 시간복잡도 개선: 현재 O(N제곱) -> O(N) 시간으로 해결해보기
+ */
+
 function solution(s) {
-  const result = [];
+  // 1. 알파벳 26개 빈도수 카운팅 배열 생성
+  const counts = new Array(26).fill(0);
+
+  // 2. 문자열 1회 순회하며 빈도수 계산 - O(N)
   for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
-      result.push(s[i]);
+    counts[s.charCodeAt(i) - 97]++;
+  }
+
+  // 3. 'a'(0)부터 'z'(25)까지 순서대로 빈도수 1인 문자 결합 - O(26)
+  let result = '';
+  for (let i = 0; i < 26; i++) {
+    if (counts[i] === 1) {
+      result += String.fromCharCode(i + 97);
     }
   }
-  return result.sort().join('');
+  
+  return result;
 }
 
 console.log(solution('abcabcadc'));
