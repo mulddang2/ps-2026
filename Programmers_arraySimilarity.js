56647bb15ca61740b7fb54b03641db084cc3cbec
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-14 13:02:15 +0900

    refactor: programmers_입문 (배열의 유사도)

--- Programmers_arraySimilarity.js
+++ Programmers_arraySimilarity.js
@@ -1,10 +1,10 @@
+/** NOTE:
+ * [x] 삼항 연산자를 반환값 활용이 아닌 부수 효과용 조건문 처럼 다룬 것 수정 -> 안티 패턴
+ * [x] 현재 시간 복잡도 (O(N * M)) -> O((N + M)) 으로 줄여보기: s2의 각 원소 M 마다 s1.includes(N)를 실행
+ */
 function solution(s1, s2) {
-  let result = 0;
-
-  for (const s2v of s2) {
-    s1.includes(s2v) ? result++ : result;
-  }
-  return result;
+  const s1Set = new Set(s1);
+  return s2.filter((v) => s1Set.has(v)).length;
 }
 
 console.log(solution(['a', 'b', 'c'], ['com', 'b', 'd', 'p', 'c']));
