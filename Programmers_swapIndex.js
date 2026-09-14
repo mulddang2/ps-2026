54f0c6e635c800308572faf8910bb02d7a8cdc19
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-14 18:01:56 +0900

    refactor: programmers_입문 (인덱스 바꾸기)

--- Programmers_swapIndex.js
+++ Programmers_swapIndex.js
@@ -1,13 +1,10 @@
+/** NOTE:
+ * [x] 배열 구조 분해 할당 활용하기
+ */
 function solution(my_string, num1, num2) {
-  const strArr = new Array(my_string.length);
-  for (let i = 0; i < my_string.length; i++) {
-    if (i === num1) {
-      strArr[num2] = my_string[i];
-    } else if (i === num2) {
-      strArr[num1] = my_string[i];
-    } else strArr[i] = my_string[i];
-  }
-  return strArr.join('')
+  const arr = [...my_string];
+  [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
+  return arr.join('');
 }
 
 console.log(solution('hello', 1, 2));
