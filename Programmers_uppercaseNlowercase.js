e34d36c88bc9a6b1f1685e9c95cf2207d6524481
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-15 16:36:46 +0900

    refactor: programmers_입문 (대문자와 소문자)

--- Programmers_uppercaseNlowercase.js
+++ Programmers_uppercaseNlowercase.js
@@ -1,13 +1,19 @@
+/** NOTE:
+ * [x] map 대신 forEach 또는 for...of 활용하기 --> 배열 변환 목적일 때 map 활용, 단순 순회 및 수정 목적은 forEach나 for...of 사용하기
+ * [x] 아스키 코드 하드코딩 교체하기
+ * [x] 불필요한 배열 생성 최소화하기
+ */
 function solution(my_string) {
-  let result = [...my_string];
-  result.map((v, i) => {
-    if (v.charCodeAt() < 97) {
-      result[i] = v.toLowerCase();
+  let result = '';
+
+  for (const char of my_string) {
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
     } else {
-      result[i] = v.toUpperCase();
+      result += char.toUpperCase();
     }
-  });
-  return result.join('');
+  }
+  return result;
 }
 
 console.log(solution('cccCCC'));
