72b624b67cf405a90810e7e2ec11002f029cafd2
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-15 19:21:23 +0900

    fix: programmers_입문 (삼각형의 완성조건 1)

--- Programmers_triangleCompletionCondition1.js
+++ Programmers_triangleCompletionCondition1.js
@@ -1,19 +1,13 @@
+/** NOTE:
+ * [x] 초기화 및 루프 분기 오류 --> i = 0 일 때, sides[0] > sides[0] 조건이 false 여서 else로 넘어감 (ex. [3, 2, 1] 케이스 실패함)
+ *[x] sort 활용하기
+ */
 function solution(sides) {
-  let maxNum = sides[0];
-  let sum = 0;
-
-  for (let i = 0; i < sides.length; i++) {
-    if (sides[i] > maxNum) {
-      maxNum = sides[i];
-    } else {
-      sum += sides[i];
-    }
-  }
-
-  return maxNum < sum ? 1 : 2;
+  sides.sort((a, b) => a - b);
+  return sides[2] < sides[0] + sides[1] ? 1 : 2;
 }
 
-console.log(solution([1, 2, 3]));
+console.log(solution([3, 2, 1]));
 console.log(solution([5, 5, 5]));
 console.log(solution([3, 6, 2]));
 console.log(solution([222, 72, 111]));
