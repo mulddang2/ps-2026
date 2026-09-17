c42b05bee7400ec31a310d2ab424feac681b0db8
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-17 11:41:41 +0900

    feat: programmers_입문 (A로 B 만들기)
    - sort, join 활용

--- Programmers_turnAIntoB.js
+++ Programmers_turnAIntoB.js
@@ -1,8 +1,12 @@
 function solution(before, after) {
-  const reverseStr = [...before].reverse().join('');
-  return +(reverseStr === after);
+  // 두 문자열이 순서만 다르고 동일한 종류와 갯수의 문자가 있는지
+  // sort 활용
+  const beforeSort = [...before].sort().join('');
+  const afterSort = [...after].sort().join('');
+
+  return beforeSort === afterSort;
 }
 
 console.log(solution('olleh', 'hello'));
-console.log(solution('allpe', 'apple'));
-console.log(solution('leel', 'leel'));
+console.log(solution('care', 'race'));
+// console.log(solution('allpe', 'apple'));
