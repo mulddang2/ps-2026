e90f05754a92e1cd8108105bc0297b983e3dedd9
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-18 14:06:04 +0900

    refactor: programmers_입문 (평행)
    - 핼퍼함수로 분리

--- programmers-introduction/parallel.js
+++ programmers-introduction/parallel.js
@@ -1,15 +1,23 @@
+/** NOTE:
+ * [x] 인덱스 직접 접근 대신 의미 있는 병수명 사용하기 -- 구조분해할당 사용
+ * [x] 두 직선의 평행 여부를 대각선 곱으로 판별
+ * [x] 반복 연산식 헬퍼 함수로 추상화하기 -- 인덱스 오타방지
+ *
+ */
 function solution(dots) {
   // 1. 배열의 각 점을 A, B, C, D로 둔다.
-  const A = dots[0],
-    B = dots[1],
-    C = dots[2],
-    D = dots[3];
-  // 2. 점을 짝지어 두 직선을 만드는 경우의 수 ( 기울기 체크 하고 경우의 수 체크)
-  return (B[1] - A[1]) * (D[0] - C[0]) === (B[0] - A[0]) * (D[1] - C[1]) ||
-    (C[1] - A[1]) * (D[0] - B[0]) === (C[0] - A[0]) * (D[1] - B[1]) ||
-    (D[1] - A[1]) * (C[0] - B[0]) === (D[0] - A[0]) * (C[1] - B[1])
-    ? 1
-    : 0;
+  const [A, B, C, D] = dots;
+
+  // 2. 두 직선 (p1-p2), (p3-p4)의 평행 여부를 대각선 곱으로 판별
+  const isParallel = (p1, p2, p3, p4) =>
+    (p2[1] - p1[1]) * (p4[0] - p3[0]) === (p4[1] - p3[1]) * (p2[0] - p1[0]);
+
+  // 3. 가능한 3가지 직성 쌍 조합 검사
+  if (isParallel(A, B, C, D)) return 1;
+  if (isParallel(A, C, B, D)) return 1;
+  if (isParallel(B, C, A, D)) return 1;
+
+  return 0;
 }
 
 console.log(
