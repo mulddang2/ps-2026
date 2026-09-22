6def2220728b0e60385775885174705de3643f30
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-22 15:28:51 +0900

    fail: programmers_입문 (숨어있는 숫자의 덧셈 2)
    - 일부 테스트 케이스 실패

---
+++ programmers-introduction/additionOfHiddenNumbers.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+  const strNum = my_string.replace(/[a-z]/gi, ' ');
+
+  const filterNum = strNum.split(' ').filter((v) => !(v === ''));
+
+  return filterNum.length === 0 ? 0 : filterNum.reduce((acc, cur) => +acc + +cur);
+}
+
+console.log(solution('aAb1B2cC34oOp'));
+console.log(solution('aopoOpppppppp'));
+console.log(solution('1a2b3c4d123Z'));
