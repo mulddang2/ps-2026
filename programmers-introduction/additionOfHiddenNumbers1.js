bf4dadd66e7bf0636c164ba08a2f22d45673c35b
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-22 17:12:13 +0900

    feat: programmers_입문 (숨어있는 숫자의 덧셈 1)

---
+++ programmers-introduction/additionOfHiddenNumbers1.js
@@ -0,0 +1,6 @@
+function solution(my_string) {
+  return my_string.match(/\d/g).reduce((acc, cur) => acc + Number(cur), 0);
+}
+
+console.log(solution('aAb1B2cC34oOp'));
+console.log(solution('1a2b3c4d123'));
