f4322d994c0b29572f8ddacb3592cc9657c929b6
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-22 17:35:33 +0900

    refactor: programmers_입문 (문자열 정렬하기 1)

--- programmers-introduction/stringSort1.js
+++ programmers-introduction/stringSort1.js
@@ -1,7 +1,12 @@
+/** NOTE:
+ * [x] 문제 제한 사항에 "숫자가 한개 이상 포함되어 있습니다" 라는 조건 있으므로 || [] 예외처리 생략 가능
+ * [x] 정렬 수행 전 .map(Number)로 숫자로 바꿔 sort 내부 비교 연산 시, 암묵적 형변환 연산 횟수 단축 가능
+ */
 function solution(my_string) {
-  return (my_string.match(/\d/g) || [])
-    .sort((a, b) => a - b)
-    .map((v) => Number(v));
+  return my_string
+    .match(/\d/g)
+    .map(Number)
+    .sort((a, b) => a - b);
 }
 
 console.log(solution('hi12392'));
