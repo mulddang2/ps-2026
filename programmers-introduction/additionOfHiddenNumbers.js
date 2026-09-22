369ee7fdb296618f710a2720451b92967810f804
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-22 15:45:31 +0900

    refactor: programmers_입문 (숨어있는 숫자의 덧셈 2)
    - 정규식 패턴 수정, 연산 횟수 개선

--- programmers-introduction/additionOfHiddenNumbers.js
+++ programmers-introduction/additionOfHiddenNumbers.js
@@ -1,9 +1,13 @@
+/** NOTE:
+ * [x] reduce 초기값 누락 및 불필요한 삼항 연산자 --> reduce에 초기값 0을 지정하면 빈 배열 경우에도 에러없이 0 반환 하기 때문에 삼항연산자 조건문은 불필요함.
+ */
 function solution(my_string) {
-  const strNum = my_string.replace(/[a-z]/gi, ' ');
-
-  const filterNum = strNum.split(' ').filter((v) => !(v === ''));
-
-  return filterNum.length === 0 ? 0 : filterNum.reduce((acc, cur) => +acc + +cur , 0);
+  // 기존 replace + split + filter 3단계를 match(/\d+/g) 1회 연산으로 대체
+  // 숫자가 없는 경우 match결과가 null이 되는 엣지케이스는 || [] 으로 안전하게 처리하여 reduce 초기값 0을 통해 빈 배열 처리와 누적합 계산 한번에 해결한다.
+  return (my_string.match(/\d+/g) || []).reduce(
+    (acc, cur) => acc + Number(cur),
+    0,
+  );
 }
 
 console.log(solution('aAb1B2cC34oOp'));
