b54ec4c3226aeee57ef9184393d4ba29be178cdd
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2026-09-22 17:05:47 +0900

    feat: programmers_입문 (배열 원소의 길이)

---
+++ programmers-introduction/elementLengthOfArray.js
@@ -0,0 +1,6 @@
+function solution(strlist) {
+  return strlist.map((v) => v.length);
+}
+
+console.log(solution(['We', 'are', 'the', 'world!']));
+console.log(solution(['I', 'Love', 'Programmers.']));
